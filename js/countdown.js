ea91e803ae9d094a6807a558a828676166378787
migraduacion <migraduacion95@gmail.com>
2025-05-16 23:30:50 -0600

    Add files via upload

--- js/countdown.js
+++ js/countdown.js
@@ -2,7 +2,7 @@ simplyCountdown('#cuenta', {
 	year: 2025, // required
 	month: 7, // required
 	day: 11, // required
-	hours: 9, // Default is 0 [0-23] integer
+	hours: 14, // Default is 0 [0-23] integer
 	minutes: 0, // Default is 0 [0-59] integer
 	seconds: 0, // Default is 0 [0-59] integer
 	words: { //words displayed into the countdown
